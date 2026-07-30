85ed282fd2eb98a84069edba627037a825f8e068
mitchellveix <mitchv87@gmail.com>
2026-07-30 11:26:30 -0400

    adding logo, updating color, adding to header

--- app/public/wp-content/themes/vinyltech/functions.php
+++ app/public/wp-content/themes/vinyltech/functions.php
@@ -1,1 +1,15 @@
 <?php
+
+function vinyltech_styles(){
+
+    wp_enqueue_style(
+        'vinyltech-header',
+        get_template_directory_uri() . '/assets/css/header.css'
+    );
+
+}
+
+add_action(
+    'wp_enqueue_scripts',
+    'vinyltech_styles'
+);

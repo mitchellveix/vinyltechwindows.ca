ae46c6e785502304e639d3476b8a24d5eb8e9df6
mitchellveix <mitchv87@gmail.com>
2026-07-31 09:55:03 -0400

    formatting hero pattern code

--- app/public/wp-content/themes/vinyltech/patterns/hero.php
+++ app/public/wp-content/themes/vinyltech/patterns/hero.php
@@ -7,22 +7,117 @@
  */
 ?>
 
-<!-- wp:cover {"url":"http://localhost:10003/wp-content/uploads/2026/07/STOCK-REPLACE-front-page-hero-1024x683.jpg","id":13,"dimRatio":90,"isUserOverlayColor":true,"focalPoint":{"x":0.45,"y":0.21},"minHeight":500,"customGradient":"linear-gradient(90deg,rgb(255,255,255) 26%,rgb(254,254,255) 42%,rgba(155,81,224,0) 100%)","sizeSlug":"large","layout":{"type":"constrained","contentSize":"85%","wideSize":"100%"}} -->
-<div class="wp-block-cover" style="min-height:500px"><img class="wp-block-cover__image-background wp-image-13 size-large" alt="" src="http://localhost:10003/wp-content/uploads/2026/07/STOCK-REPLACE-front-page-hero-1024x683.jpg" style="object-position:45% 21%" data-object-fit="cover" data-object-position="45% 21%"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient" style="background:linear-gradient(90deg,rgb(255,255,255) 26%,rgb(254,254,255) 42%,rgba(155,81,224,0) 100%)"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"grid","columnCount":2}} -->
-<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-black-color has-text-color has-link-color has-x-large-font-size">Premium Windows &amp; Doors</h2>
-<!-- /wp:heading -->
+<!-- wp:cover {
+	"url":"http://localhost:10003/wp-content/uploads/2026/07/STOCK-REPLACE-front-page-hero-1024x683.jpg",
+	"id":13,
+	"dimRatio":90,
+	"isUserOverlayColor":true,
+	"focalPoint":{"x":0.45,"y":0.21},
+	"minHeight":500,
+	"customGradient":"linear-gradient(90deg,rgb(255,255,255) 26%,rgb(254,254,255) 42%,rgba(155,81,224,0) 100%)",
+	"sizeSlug":"large",
+	"layout":{
+		"type":"constrained",
+		"contentSize":"85%",
+		"wideSize":"100%"
+	}
+} -->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
-<p class="has-black-color has-text-color has-link-color">Serving Southwestern Ontario Serving Southwestern Ontario Serving Southwestern Ontario Serving Southwestern Ontario</p>
-<!-- /wp:paragraph -->
+<div class="wp-block-cover" style="min-height:500px">
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Request Quote</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div></div>
+	<img
+		class="wp-block-cover__image-background wp-image-13 size-large"
+		alt=""
+		src="http://localhost:10003/wp-content/uploads/2026/07/STOCK-REPLACE-front-page-hero-1024x683.jpg"
+		style="object-position:45% 21%"
+		data-object-fit="cover"
+		data-object-position="45% 21%"
+	/>
+
+	<span
+		aria-hidden="true"
+		class="wp-block-cover__background has-background-dim-90 has-background-dim wp-block-cover__gradient-background has-background-gradient"
+		style="background:linear-gradient(90deg,rgb(255,255,255) 26%,rgb(254,254,255) 42%,rgba(155,81,224,0) 100%)">
+	</span>
+
+	<div class="wp-block-cover__inner-container">
+
+		<!-- wp:group {"layout":{"type":"grid","columnCount":2}} -->
+
+		<div class="wp-block-group">
+
+			<!-- wp:group {"layout":{"type":"constrained"}} -->
+
+			<div class="wp-block-group">
+
+				<!-- wp:heading {
+					"style":{
+						"elements":{
+							"link":{
+								"color":{
+									"text":"var:preset|color|black"
+								}
+							}
+						}
+					},
+					"textColor":"black",
+					"fontSize":"x-large"
+				} -->
+
+				<h2 class="wp-block-heading has-black-color has-text-color has-link-color has-x-large-font-size">
+					Premium Windows &amp; Doors
+				</h2>
+
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {
+					"style":{
+						"elements":{
+							"link":{
+								"color":{
+									"text":"var:preset|color|black"
+								}
+							}
+						}
+					},
+					"textColor":"black"
+				} -->
+
+				<p class="has-black-color has-text-color has-link-color">
+					Serving Southwestern Ontario Serving Southwestern Ontario
+					Serving Southwestern Ontario Serving Southwestern Ontario
+				</p>
+
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+
+				<div class="wp-block-buttons">
+
+					<!-- wp:button -->
+
+					<div class="wp-block-button">
+						<a class="wp-block-button__link wp-element-button">
+							Request Quote
+						</a>
+					</div>
+
+					<!-- /wp:button -->
+
+				</div>
+
+				<!-- /wp:buttons -->
+
+			</div>
+
+			<!-- /wp:group -->
+
+		</div>
+
+		<!-- /wp:group -->
+
+	</div>
+
+</div>
+
 <!-- /wp:cover -->

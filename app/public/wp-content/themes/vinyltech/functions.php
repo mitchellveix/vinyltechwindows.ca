e5883986ac09b4a6fc1c67233748b6c7feeaf61c
mitchellveix <mitchv87@gmail.com>
2026-07-31 09:52:42 -0400

    creating hero pattern

--- app/public/wp-content/themes/vinyltech/functions.php
+++ app/public/wp-content/themes/vinyltech/functions.php
@@ -13,3 +13,39 @@ add_action(
     'wp_enqueue_scripts',
     'vinyltech_styles'
 );
+
+function vinyltech_register_patterns(){
+
+    register_block_pattern_category(
+        'vinyltech',
+        array(
+            'label' => 'VinylTech'
+        )
+    );
+
+}
+
+add_action(
+    'init',
+    'vinyltech_register_patterns'
+);
+
+function vinyltech_register_pattern_files(){
+
+    register_block_pattern(
+        'vinyltech/hero',
+        array(
+            'title' => 'VinylTech Hero',
+            'content' => file_get_contents(
+                get_template_directory() 
+                . '/patterns/hero.php'
+            )
+        )
+    );
+
+}
+
+add_action(
+    'init',
+    'vinyltech_register_pattern_files'
+);
